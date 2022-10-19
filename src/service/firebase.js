a2a59aa831debd37cec4d51369d64358c3446638
wonsikgo <45756634+wonsikgo@users.noreply.github.com>
2022-10-19 23:47:01 +0900

    auth service 추가

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -9,7 +9,7 @@ import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
   apiKey: REACT_APP_FIREBASE_API_KEY,
   authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: REACT_APP_FIREBASE_DB_URL,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
 };
 
 // Initialize Firebase
