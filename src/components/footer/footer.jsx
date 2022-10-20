4c4d1fc78a1476b8c594847a08a0465c8131d2b2
wonsickgo <45756634+wonsikgo@users.noreply.github.com>
2022-10-20 15:57:27 +0900

    로그인 스타일링

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import styles from "./footer.module.css";
 
-const Footer = () => {
-  return <div>Footer</div>;
-};
+const Footer = () => (
+  <footer className={styles.footer}>
+    <p className={styles.title}>Code your dream</p>
+  </footer>
+);
 
 export default Footer;
