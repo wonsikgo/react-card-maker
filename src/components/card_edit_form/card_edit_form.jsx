992894b421b57b0ce7ff5af44124d39ca8a0ea5b
wonsickgo <45756634+wonsikgo@users.noreply.github.com>
2022-10-26 17:22:47 +0900

    input value를 defaultValue로 변경

--- src/components/card_edit_form/card_edit_form.jsx
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -11,21 +11,40 @@ const CardEditForm = ({ card }) => {
 
   return (
     <form className={styles.form}>
-      <input className={styles.input} type="text" name="name" value={name} />
+      <input
+        className={styles.input}
+        type="text"
+        name="name"
+        defaultValue={name}
+      />
       <input
         className={styles.input}
         type="text"
         name="company"
-        value={company}
+        defaultValue={company}
       />
-      <select className={styles.select} name="theme" value={theme}>
+      <select className={styles.select} name="theme" defaultValue={theme}>
         <option value="light">Light</option>
         <option value="dark">Dark</option>
         <option value="colorful">Colorful</option>
       </select>
-      <input className={styles.input} type="text" name="title" value={title} />
-      <input className={styles.input} type="text" name="email" value={email} />
-      <textarea className={styles.textarea} name="message" value={message} />
+      <input
+        className={styles.input}
+        type="text"
+        name="title"
+        defaultValue={title}
+      />
+      <input
+        className={styles.input}
+        type="text"
+        name="email"
+        defaultValue={email}
+      />
+      <textarea
+        className={styles.textarea}
+        name="message"
+        defaultValue={message}
+      />
       <div className={styles.fileInput}>
         <ImageFileInput />
       </div>
