2ab215b256262b540fc543b3ccdda906891fa631
wonsickgo <45756634+wonsikgo@users.noreply.github.com>
2022-10-26 16:50:58 +0900

    Button, Image input component 추가

--- src/components/card_edit_form/card_edit_form.jsx
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Button from "../button/button";
+import ImageFileInput from "../image_file_input/image_file_input";
 import styles from "./card_edit_form.module.css";
 
 const CardEditForm = ({ card }) => {
   const { name, company, title, email, message, theme, fileName, fileURL } =
     card;
+
+  const onSubmit = () => {};
+
   return (
     <form className={styles.form}>
       <input className={styles.input} type="text" name="name" value={name} />
@@ -21,7 +26,10 @@ const CardEditForm = ({ card }) => {
       <input className={styles.input} type="text" name="title" value={title} />
       <input className={styles.input} type="text" name="email" value={email} />
       <textarea className={styles.textarea} name="message" value={message} />
-      <div className={styles.fileInput}></div>
+      <div className={styles.fileInput}>
+        <ImageFileInput />
+      </div>
+      <Button name="Delete" onClick={onSubmit} />
     </form>
   );
 };
