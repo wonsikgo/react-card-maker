0f3bd4a128a2b271de712fd0b23f8929b5ddc344
wonsikgo <45756634+wonsikgo@users.noreply.github.com>
2022-10-28 23:58:24 +0900

    이미지 업로드 모듈 생성

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.module.css";
 import App from "./app";
 import AuthService from "./service/auth_service.js";
+import ImageUploader from "./service/image_uploader";
 
 const authService = new AuthService();
+const imageUploader = new ImageUploader();
+const FileInupt = (props) => (
+  <ImageUploader {...props} imageUploader={imageUploader} />
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
