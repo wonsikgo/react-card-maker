a4b96d6d2acd24cabb3b7f01650c301ea8eb65ef
wonsickgo <45756634+wonsikgo@users.noreply.github.com>
2022-11-21 17:48:12 +0900

    firebase database sync

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -36,6 +36,18 @@ const Maker = ({ FileInput, authService, cardRepository }) => {
   };
 
   useEffect(() => {
+    if (!userId) return;
+
+    const stopSync = cardRepository.syncCards(userId, (cards) => {
+      setCards(cards);
+    });
+
+    return () => {
+      stopSync();
+    };
+  }, [userId]);
+
+  useEffect(() => {
     authService.onAuthChange((user) => {
       if (user) {
         setUserId(user.uid);
--- src/service/card_repository.js
+++ src/service/card_repository.js
@@ -11,6 +11,14 @@ class CardRepository {
     console.log(`card : ${card}`);
     firebaseApp.database().ref(`${userId}/cards/${card.id}`).remove();
   }
+  syncCards(userId, onUpdate) {
+    const ref = firebaseApp.database().ref(`${userId}/cards`);
+    ref.on("value", (snapshot) => {
+      const value = snapshot.val();
+      value && onUpdate(value);
+    });
+    return () => ref.off();
+  }
 }
 
 export default CardRepository;
